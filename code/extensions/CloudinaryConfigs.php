7f666a73872e379f432ce24140bc68a25cdaafba
Nivanka Fonseka <nivanka@silverstripers.com>
2015-12-14 11:56:04 +0530

    updated the relationships look up and updating the configs. This only happens one time now, and thats only when the site is flushed

--- code/extensions/CloudinaryConfigs.php
+++ code/extensions/CloudinaryConfigs.php
@@ -33,32 +33,67 @@ class CloudinaryConfigs extends DataExtension {
 	 * ReplaceFileFields
 	 */
 	public static function ReplaceFileFields() {
-		if(Config::inst()->get('CloudinaryConfigs', 'replace_relationships')) {
+
+		if(isset($_REQUEST['flush']) && $_REQUEST['flush'] == 'all'){
 			$arrRelationships = array('has_one', 'has_many', 'many_many');
-
 			$classes = ClassInfo::subclassesFor('DataObject');
-			foreach($classes as $className) {
-				foreach($arrRelationships as $relationType){
-					if($relation = Config::inst()->get($className, $relationType)) {
-						if(in_array('File', $relation) || in_array('Image', $relation)) {
-							$updateRelations = array();
-							foreach($relation as $field => $type){
-								$newType = $type;
-								if($type == 'File'){
-									$newType = 'CloudinaryFile';
-								}
-								if($type == 'Image'){
-									$newType = 'CloudinaryImage';
-								}
-								$updateRelations[$field] = $newType;
-							}
-							Config::inst()->update($className, $relationType, $updateRelations);
+			$configManifest = new SS_ConfigStaticManifest(BASE_PATH, false, true);
+			$static = $configManifest->getStatics();
 
+			$arrUpdated = array();
+
+
+			foreach($static as $className => $options){
+				$bUpdated = false;
+
+				if(!in_array($className, array('File', 'Image'))){
+
+					foreach($arrRelationships as $relation){
+						if(in_array($className, $classes) && isset($options[$relation]) && isset($options[$relation]['value'])){
+							foreach($options[$relation]['value'] as $field => $value){
+								if($value == 'Image'){
+									$options[$relation]['value'][$field] = 'CloudinaryImage';
+									$bUpdated = true;
+								}
+								else if ($value == 'File'){
+									$options[$relation]['value'][$field] = 'CloudinaryFile';
+									$bUpdated = true;
+								}
+							}
 						}
+					}
+
+					if($bUpdated){
+						$arrUpdated[$className] = $options;
+						foreach($arrRelationships as $relation){
+							if(isset($options[$relation]['value'])){
+								Config::inst()->update($className, $relation, $options[$relation]['value']);
+							}
+						}
+					}
+				}
+
+				if(count($arrUpdated)){
+					$cacheClass = defined('SS_MANIFESTCACHE') ? SS_MANIFESTCACHE : 'ManifestCache_File';
+					$cache = new $cacheClass('staticmanifest');
+					$pathKey = sha1(BASE_PATH);
+
+					$keysets = array();
+
+					foreach ($arrUpdated as $class => $details) {
+						$key = sha1($class);
+						$keysets[$key][$class] = $details;
+					}
+
+					foreach ($keysets as $key => $details) {
+						$cache->save($details, $pathKey.'_'.$key);
 					}
 				}
 			}
 		}
+
+
+
 	}
 
 } 
