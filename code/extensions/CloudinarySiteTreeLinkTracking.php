d83751148263233aff705944b54e8110353a5492
Nivanka Fonseka <nivanka@silverstripers.com>
2016-02-02 10:17:10 +0530

    fixed the HTML content being assigned to Markdown fields

--- code/extensions/CloudinarySiteTreeLinkTracking.php
+++ code/extensions/CloudinarySiteTreeLinkTracking.php
@@ -22,10 +22,10 @@ class CloudinarySiteTreeLinkTracking extends SiteTreeLinkTracking {
 	 *
 	 * @param string $field The name of the field on {@link @owner} to scrape
 	 */
-	public function trackLinksInField($field) {
+	public function trackLinksInField($fieldName) {
 		$record = $this->owner;
 		$linkedCloudinary = array();
-		preg_match_all('/\[cloudinary,id=\"\d*\"/i', $record->$field, $matches);
+		preg_match_all('/\[cloudinary,id=\"\d*\"/i', $record->$fieldName, $matches);
         if($matches && isset($matches[0])) {
             foreach($matches[0] as $match) {
                 $id = str_replace('"', '', str_replace('[cloudinary,id=', '', $match));
@@ -41,16 +41,94 @@ class CloudinarySiteTreeLinkTracking extends SiteTreeLinkTracking {
 		if($record->ID && $record->many_many('CloudinaryTracking') && $tracker = $record->CloudinaryTracking()) {
 			$tracker->removeByFilter(sprintf(
 				'"FieldName" = \'%s\' AND "%s" = %d',
-				$field,
+				$fieldName,
 				$tracker->getForeignKey(),
 				$record->ID
 			));
 
 			if($linkedCloudinary) foreach($linkedCloudinary as $item) {
-				$tracker->add($item, array('FieldName' => $field));
+				$tracker->add($item, array('FieldName' => $fieldName));
 			}
 		}
-		parent::trackLinksInField($field);
+
+
+
+		/// override the parent functions
+		$record = $this->owner;
+
+		$linkedPages = array();
+		$linkedFiles = array();
+
+		$htmlValue = Injector::inst()->create('HTMLValue', $record->$fieldName);
+		$links = $this->parser->process($htmlValue);
+
+		// Highlight broken links in the content.
+		foreach ($links as $link) {
+			$classStr = trim($link['DOMReference']->getAttribute('class'));
+			if (!$classStr) {
+				$classes = array();
+			} else {
+				$classes = explode(' ', $classStr);
+			}
+
+			// Add or remove the broken class from the link, depending on the link status.
+			if ($link['Broken']) {
+				$classes = array_unique(array_merge($classes, array('ss-broken')));
+			} else {
+				$classes = array_diff($classes, array('ss-broken'));
+			}
+
+			if (!empty($classes)) {
+				$link['DOMReference']->setAttribute('class', implode(' ', $classes));
+			} else {
+				$link['DOMReference']->removeAttribute('class');
+			}
+		}
+
+
+		// Populate link tracking for internal links & links to asset files.
+		foreach ($links as $link) {
+			switch ($link['Type']) {
+				case 'sitetree':
+					if ($link['Broken']) {
+						$record->HasBrokenLink = true;
+					} else {
+						$linkedPages[] = $link['Target'];
+					}
+					break;
+
+				case 'file':
+					if ($link['Broken']) {
+						$record->HasBrokenFile = true;
+					} else {
+						$linkedFiles[] = $link['Target'];
+					}
+					break;
+
+				default:
+					if ($link['Broken']) {
+						$record->HasBrokenLink = true;
+					}
+					break;
+			}
+		}
+
+
+		// Update the "LinkTracking" many_many
+		if($record->ID && $record->manyManyComponent('LinkTracking') && $tracker = $record->LinkTracking()) {
+			$tracker->removeByFilter(sprintf(
+				'"FieldName" = \'%s\' AND "%s" = %d',
+				$fieldName,
+				$tracker->getForeignKey(),
+				$record->ID
+			));
+
+			if($linkedPages) foreach($linkedPages as $item) {
+				$tracker->add($item, array('FieldName' => $fieldName));
+			}
+		}
+
+
 	}
 
 	/**
