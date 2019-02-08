e824bb5a3e84a89ad1cab79ef92259391ba9da3c
Lee Bradley <lee.bradley@mademedia.co.uk>
2019-02-08 17:23:25 +0000

    Removes the `sync` button from admin
    This is better handled using the Notification URL in Cloudinary Settings
    > Upload

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,14 +1,6 @@
-import CloudinaryEnhancedGalleryToolbar from './components/CloudinaryEnhancedGalleryToolbar';
 import CloudinaryPreviewImageField from './components/CloudinaryPreviewImageField';
 
 import Injector from 'lib/Injector';
-
-Injector.transform(
-    'cloudinary-sync-button',
-    updater => {
-        updater.component('GalleryToolbar', CloudinaryEnhancedGalleryToolbar);
-    }
-);
 
 Injector.transform(
     'cloudinary-preview-image-field',
