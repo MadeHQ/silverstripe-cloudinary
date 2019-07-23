a06c222b8bc9bba25bb7572118408d7187bc2608
hirenpatel <hiren.patel@mademedia.co.uk>
2019-07-23 11:32:12 +0100

    Starting a fresh module

--- _config.php
+++ _config.php
@@ -1,16 +1,1 @@
 <?php
-
-/**
- * Silverstripe Cloudinary Module configuration file.
- *
- * PHP version >=5.6.0
- *
- * For full copyright and license information, please view the
- * LICENSE.md file that was distributed with this source code.
- *
- * @package Vendor\Module
- * @author Made Media <developers@mademedia.co.uk>
- * @copyright 2017 Made Media Ltd.
- * @license https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @link https://github.com/MadeHQ/silverstripe-cloudinary.git
- */
---
+++ client/src/js/main.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const cloudinary = require('./cloudinary');
