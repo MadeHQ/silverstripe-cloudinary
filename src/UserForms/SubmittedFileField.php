a1b984fff0abadb594fc576e28d4a88e67dec800
Lee Bradley <lee.bradley@made.media>
2022-09-27 11:57:45 +0100

    User Forms: Add a check that UserForm classes exist to prevent errors
    This means that there is not a dependency on the UserForms module

--- src/UserForms/SubmittedFileField.php
+++ src/UserForms/SubmittedFileField.php
@@ -6,33 +6,36 @@ use Cloudinary\Cloudinary;
 use Cloudinary\Configuration\Configuration;
 use SilverStripe\UserForms\Model\Submission\SubmittedFileField as SubmissionSubmittedFileField;
 
-class SubmittedFileField extends SubmissionSubmittedFileField
-{
-    /**
-     * @var Cloudinary
-     */
-    protected static $cloudinary_instance;
+if (class_exists(SubmissionSubmittedFileField::class)) {
 
-    /**
-     * @inheritdoc
-     */
-    public function getLink()
+    class SubmittedFileField extends SubmissionSubmittedFileField
     {
-        $config = [
-            'resource_type' => 'raw',
-            'attachment' => true,
-        ];
-        return static::cloudinary_instance()->uploadApi()->privateDownloadUrl($this->Value, '', $config);
-    }
+        /**
+         * @var Cloudinary
+         */
+        protected static $cloudinary_instance;
 
-    /**
-     * @return Cloudinary
-     */
-    public static function cloudinary_instance()
-    {
-        if (!static::$cloudinary_instance) {
-            static::$cloudinary_instance = new Cloudinary(Configuration::instance());
+        /**
+         * @inheritdoc
+         */
+        public function getLink()
+        {
+            $config = [
+                'resource_type' => 'raw',
+                'attachment' => true,
+            ];
+            return static::cloudinary_instance()->uploadApi()->privateDownloadUrl($this->Value, '', $config);
         }
-        return static::$cloudinary_instance;
+
+        /**
+         * @return Cloudinary
+         */
+        public static function cloudinary_instance()
+        {
+            if (!static::$cloudinary_instance) {
+                static::$cloudinary_instance = new Cloudinary(Configuration::instance());
+            }
+            return static::$cloudinary_instance;
+        }
     }
 }
